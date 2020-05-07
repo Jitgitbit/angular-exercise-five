0c47d1426d2a43bf59754af74c03a1ee88afcc23
Jitgitbit <rowowolf@gmail.com>
2020-05-08 00:17:02 +0200

    Injecting Services into Services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -6,7 +6,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService, AccountsService]
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
@@ -21,7 +21,7 @@ export class AccountComponent {
   onSetTo(status: string) {
     // this.statusChanged.emit({id: this.id, newStatus: status});
     this.accountsService.updateStatus(this.id, status)
-    this.loggingService.logStatusChange(status);
+    // this.loggingService.logStatusChange(status);
     // console.log('A server status changed, new status: ' + status);
   }
 }
